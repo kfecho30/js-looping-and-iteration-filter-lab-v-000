18b53b45ccf44abfa9f7a850aa194371722a4724
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 16:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function findMatching(arr, name){
 
 function fuzzyMatch(arr, letters){
   return arr.filter(function (fletters){
+    debugger;
     return fletters.slice(0, letters.length).toUpperCase === letters.toUpperCase()
   });
 }
