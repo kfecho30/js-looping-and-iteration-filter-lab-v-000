a16fb07f27ffca451ccb6d44c6bbf72bbf171b80
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 16:33:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Code your solution in this file
 function findMatching(arr, name){
-  return arr.filter(function (dname) {return name.toUpperCase() === dname.toUpperCase()});
+  return arr.filter(function (dname) {
+    return name.toUpperCase() === dname.toUpperCase()
+  });
+}
+
+function fuzzyMatch(){
+  
 }
