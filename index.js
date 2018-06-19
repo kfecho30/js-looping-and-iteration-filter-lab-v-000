099ee91742919489508f1dcd590b7818220353f7
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 16:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(arr, name){
-  let result = arr.filter(name === arr.name);
+  let result = arr.filter(function (name) {return name === arr.name});
   return result;
 }
